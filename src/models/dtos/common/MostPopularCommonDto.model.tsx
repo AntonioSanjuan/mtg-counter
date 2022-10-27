d202112868eb306679765ebf81e1edbb56a4cff6
Tony Sanjuan <tonysanj13@gmail.com>
2022-10-27 16:47:03 +0200

    fix: removed unused app state

--- src/models/dtos/common/MostPopularCommonDto.model.tsx
+++
@@ -1,6 +0,0 @@
-export interface MosPopularCommonDto<T> {
-  status: string;
-  copyright: string;
-  num_results: number;
-  results: T;
-}
--- src/state/rootState.tsx
+++ src/state/rootState.tsx
@@ -1,7 +1,5 @@
 import { combineReducers, createStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { dataReducer } from './data/data.reducer';
-import { DataState } from './data/models/appData.state';
 import gameReducer from './game/game.reducer';
 import { GameState } from './game/models/appGame.state';
 import { layoutReducer } from './layout/layout.reducer';
@@ -12,14 +10,12 @@ import { userReducer } from './user/user.reducer';
 export interface AppRootState {
     user: UserState;
     layout: LayoutState;
-    data: DataState;
     game: GameState;
 }
 
 export const combinedReducers = combineReducers({
   user: userReducer,
   layout: layoutReducer,
-  data: dataReducer,
   game: gameReducer,
 });
 
