0e21c6b0c2686d90ddfd57ba11c731c737fc63e4
Tony Sanjuan <tonysanj13@gmail.com>
2023-02-10 12:05:16 +0100

    fix: non-updated file

--- src/utils/factories/gameFactory/gameFactory.tsx
+++ src/utils/factories/gameFactory/gameFactory.tsx
@@ -1,7 +1,7 @@
 import { FirebaseGameDto } from '../../../models/dtos/firebaseStore/firebaseGameSettings.model';
 import { Lifes } from '../../../models/internal/types/LifeEnum.model';
 import { NumberOfPlayers } from '../../../models/internal/types/NumberOfPlayerEnum.model';
-import { getDefaultPlayers } from '../../playerFactory/playerFactory';
+import { getDefaultPlayers } from '../playerFactory/playerFactory';
 
 export const getNewGame = (
   initialLifes = Lifes.Fourty,
