ef6c103d0a9c95b83b59836032795ed67459599b
Tony Sanjuan <tonysanj13@gmail.com>
2023-04-08 18:07:43 +0200

    test: restored old test

--- src/hooks/user/userHook.unit.test.tsx
+++ src/hooks/user/userHook.unit.test.tsx
@@ -53,6 +53,113 @@ describe('<useUser />', () => {
     firebaseAuthServiceMock.reset();
   });
 
+  // it('should create', () => {
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+
+  //   expect(result.current).toBeDefined();
+  // });
+
+  // it('login should request firebaseLogin', async () => {
+  //   expect(firebaseAuthServiceMock.firebaseLoginSpy).not.toHaveBeenCalled();
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+
+  //   await act(async () => {
+  //     await result.current.login({ username: 'a@b.com', password: '' });
+  //   });
+
+  //   expect(firebaseAuthServiceMock.firebaseLoginSpy).toHaveBeenCalled();
+  // });
+
+  // it('login failure should set error flag true value', async () => {
+  //   const error = { errorDesc: 'test_errorDEsc' };
+  //   firebaseAuthServiceMock.firebaseLoginSpy.mockRejectedValue(error);
+
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+
+  //   await act(async () => {
+  //     await result.current.login({ username: 'a@b.com', password: '' }).catch((e) => {
+  //       // eslint-disable-next-line jest/no-conditional-expect
+  //       expect(e).toEqual(error);
+  //     });
+  //   });
+  //   expect(result.current.error).toBeTruthy();
+  //   expect(result.current.loading).toBeFalsy();
+  // });
+
+  // it('loginWithGoogle should request firebaseGoogleLogin', async () => {
+  //   expect(firebaseAuthServiceMock.firebaseGoogleLoginSpy).not.toHaveBeenCalled();
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+
+  //   await act(async () => {
+  //     await result.current.loginWithGoogle();
+  //   });
+
+  //   expect(firebaseAuthServiceMock.firebaseGoogleLoginSpy).toHaveBeenCalled();
+  // });
+
+  // it('loginWithGoogle failure should set error flag true value', async () => {
+  //   const error = { errorDesc: 'test_errorDEsc' };
+  //   firebaseAuthServiceMock.firebaseGoogleLoginSpy.mockRejectedValue(error);
+
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+
+  //   await act(async () => {
+  //     await result.current.loginWithGoogle().catch((e) => {
+  //       // eslint-disable-next-line jest/no-conditional-expect
+  //       expect(e).toEqual(error);
+  //     });
+  //   });
+  //   expect(result.current.error).toBeTruthy();
+  //   expect(result.current.loading).toBeFalsy();
+  // });
+
+  // it('logout should request firebaseLogout setvice function', async () => {
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+
+  //   expect(firebaseAuthServiceMock.firebaseLogoutSpy).not.toHaveBeenCalled();
+  //   await act(async () => {
+  //     await result.current.logout();
+  //   });
+  //   expect(firebaseAuthServiceMock.firebaseLogoutSpy).toHaveBeenCalled();
+  // });
+
+  // it('signUp should request firebaseSignUp service function', async () => {
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+  //   const userName = 'test_username';
+  //   const userPass = 'test_userpass';
+
+  //   expect(firebaseAuthServiceMock.firebaseSignUpSpy).not.toHaveBeenCalled();
+
+  //   await act(async () => {
+  //     await result.current.signUp({ username: userName, password: userPass });
+  //   });
+
+  //   expect(firebaseAuthServiceMock.firebaseSignUpSpy).toHaveBeenCalledWith(userName, userPass);
+  // });
+
+  // it('signUp should request setUserSettings hook function', async () => {
+  //   const sutGameSettingsId = 'testGameSettingsId'
+  //   const { result } = renderHook(() => useUser(), { wrapper });
+
+  //   expect(firebaseAuthServiceMock.firebaseSignUpSpy).not.toHaveBeenCalled();
+  //   const userSettings = { lang: Language.French, darkMode: true } as FirebaseUserSettingsDto;
+
+
+  //   mock().setGameSettings.mockResolvedValue({ 
+  //     id: sutGameSettingsId,
+  //     data: () => {} 
+  //   })
+
+  //   await act(async () => {
+  //     await useUserStore.dispatch(setUserSettingsAction(userSettings));
+  //   });
+
+  //   await act(async () => {
+  //     await result.current.signUp({ username: '', password: '' });
+  //   });
+
+  //   expect(useUserSettingsMock().setUserSettings).toHaveBeenCalledWith(userSettings, sutGameSettingsId);
+  // });
 
   it('signUp should request setGameSettings hook function with default value', async () => {
     const { result } = renderHook(() => useUser(), { wrapper });
