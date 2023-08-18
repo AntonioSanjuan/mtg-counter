2d9d2c4f998f7828becb4f3cf2922a990ddf34d2
Antonio Sanjuan <tonysanj13@gmail.com>
2023-08-18 09:16:01 +0200

    refactor: refactored and styled DeckInfo component

--- src/components/DeckInfo/DeckInfo.tsx
+++ src/components/DeckInfo/DeckInfo.tsx
@@ -1,7 +1,9 @@
 import './DeckInfo.scss';
 import { FirebaseDeckDto } from '../../models/dtos/firebaseStore/firebaseDeckCollection.model';
-import { selectHistoricGamesByDeck, selectHistoricGamesWinnedByDeck } from '../../state/historicGames/historicGames.selectors';
-import { HistoricGamesState } from '../../state/historicGames/models/appHistoricGames.state';
+import {
+  selectHistoricGamesByDeck,
+  selectHistoricGamesWinnedByDeck,
+} from '../../state/historicGames/historicGames.selectors';
 import { useAppSelector } from '../../hooks/state/appStateHook';
 import { GameState } from '../../state/game/models/appGame.state';
 
@@ -11,23 +13,28 @@ function DeckInfo({ deck } : {deck: FirebaseDeckDto}) {
 
   return (
     <div className="DeckInfo_MainContainer">
-      <div className="DeckInfo_Details">
-        <p className="app_font_l app_font_noMargin">Deck Name:</p>
-        <p className="app_font_m app_font_noMargin">{deck.name}</p>
-      </div>
-      <div className="DeckInfo_Details">
-        <p className="app_font_l app_font_noMargin">Commander Name:</p>
-        <p className="app_font_m app_font_noMargin">{deck.commanderName}</p>
-      </div>
-      <div className="DeckInfo_Statistics">
-        <p className="app_font_l app_font_noMargin">Games: </p>
-        <div className="DeckInfo_Details">
-          <p className="app_font_l app_font_noMargin">Played: </p>
-          <p className="app_font_m app_font_noMargin">{playedGames.length}</p>
+      <div className="DeckInfo_InfoContainer">
+        <div className="DeckInfo_Info">
+          <p className="app_font_l app_font_noMargin">Deck:</p>
+          <p className="app_font_m app_font_noMargin">{deck.name}</p>
         </div>
-        <div className="DeckInfo_Details">
-          <p className="app_font_l app_font_noMargin">Winned: </p>
-          <p className="app_font_m app_font_noMargin">{winnedGames.length}</p>
+        <div className="DeckInfo_Info">
+          <p className="app_font_l app_font_noMargin">Commander:</p>
+          <p className="app_font_m app_font_noMargin">{deck.commanderName}</p>
+        </div>
+      </div>
+      <hr />
+      <div className="DeckInfo_StatisticsContainer">
+        <h3 className="app_font_xl">Games</h3>
+        <div className="DeckInfo_Statistics">
+          <div className="DeckInfo_Statistic">
+            <p className="app_font_l app_font_noMargin">Played: </p>
+            <p className="app_font_m app_font_noMargin">{playedGames.length}</p>
+          </div>
+          <div className="DeckInfo_Statistic">
+            <p className="app_font_l app_font_noMargin">Winned: </p>
+            <p className="app_font_m app_font_noMargin">{winnedGames.length}</p>
+          </div>
         </div>
       </div>
     </div>
